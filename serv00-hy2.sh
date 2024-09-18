d06a04bd768330dbd45433aa462d78ce1b68c89e
cooler1987 <yz101101@outlook.com>
2024-09-18 19:36:51 +0800

    Update serv00-hy2.sh

--- serv00-hy2.sh
+++ serv00-hy2.sh
@@ -143,8 +143,9 @@ echo -e "\e[1;32mHysteria2安装成功\033[0m\n"
 echo -e "\e[1;33mV2rayN 或 Nekobox、小火箭等直接导入,跳过证书验证需设置为true\033[0m\n"
 echo -e "\e[1;32mhysteria2://$UUID@$HOST_IP:$PORT/?sni=www.bing.com&alpn=h3&insecure=1#$ISP\033[0m\n"
 echo -e "\e[1;33mSurge\033[0m"
-echo -e "\e[1;32m$HOSTNAME = hysteria2, $HOST_IP, $PORT, password = $UUID, skip-cert-verify=true, sni=www.bing.com\033[0m\n"
+echo -e "\e[1;32m$ISP = hysteria2, $HOST_IP, $PORT, password = $UUID, skip-cert-verify=true, sni=www.bing.com\033[0m\n"
 echo -e "\e[1;33mClash\033[0m"
+
 cat << EOF
 - name: $HOSTNAME
   type: hysteria2
