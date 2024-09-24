ed1fdafa1d3ecb0626e2b2c1b93217a535a899ae
cooler1987 <yz101101@outlook.com>
2024-09-24 21:25:24 +0800

    Update serv00-tuic.sh

--- serv00-tuic.sh
+++ serv00-tuic.sh
@@ -9,14 +9,17 @@ export PORT=${PORT:-'0000'}
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 
+# 设置工作目录
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USER,,}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR" && cd "$WORKDIR")
 ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9
 
-# Download Dependency Files
+# 下载依赖文件
 clear
 echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
 ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+
+# 根据系统架构下载对应的文件
 if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
     FILE_INFO=("https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd.sha256sum web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
 elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
@@ -25,6 +28,7 @@ else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
+
 declare -A FILE_MAP
 generate_random_name() {
     local chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
@@ -74,10 +78,10 @@ for entry in "${FILE_INFO[@]}"; do
 done
 wait
 
-# Generate cert
+# 生成 SSL 证书
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout $WORKDIR/server.key -out $WORKDIR/server.crt -subj "/CN=bing.com" -days 36500
 
-# Generate configuration file
+# 生成 TUIC 配置文件
 cat > config.json <<EOL
 {
   "server": "[::]:$PORT",
@@ -101,7 +105,7 @@ cat > config.json <<EOL
 }
 EOL
 
-# running files
+# 运行函数
 run() {
   if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
     tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
@@ -127,6 +131,7 @@ run() {
 
 run
 
+# 获取 IP 并输出 TUIC 链接
 HOST_IP=$(curl -s ifconfig.me || echo "0.0.0.0")
 echo -e "\n\e[1;35m以下是您的链接:\e[0m\n"
 echo -e "\e[1;32mtuic://$UUID@$HOST_IP:$PORT#$ISP\e[0m\n"
