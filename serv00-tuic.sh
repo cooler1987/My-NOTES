efd5700215865751e6e874a552aa85b825712fc5
cooler1987 <yz101101@outlook.com>
2024-09-24 21:45:18 +0800

    Update serv00-tuic.sh

--- serv00-tuic.sh
+++ serv00-tuic.sh
@@ -9,35 +9,75 @@ export PORT=${PORT:-'0000'}
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 
-# 设置工作目录
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USER,,}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR" && cd "$WORKDIR")
 ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9
 
-# 下载 TUIC 依赖文件
+# Download Dependency Files
 clear
 echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
-ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR"
-
-# 根据系统架构下载对应的文件
-if [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ]; then
-    TUIC_URL="https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd"
-elif [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-    TUIC_URL="https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-arm64-unknown-freebsd"
+ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+    FILE_INFO=("https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd.sha256sum web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
+elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+    FILE_INFO=("https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd web" "https://github.com/eooce/test/releases/download/freebsd/swith npm")
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
 fi
+declare -A FILE_MAP
+generate_random_name() {
+    local chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
+    local name=""
+    for i in {1..6}; do
+        name="$name${chars:RANDOM%${#chars}:1}"
+    done
+    echo "$name"
+}
 
-# 下载 TUIC 文件
-TUIC_FILE="$DOWNLOAD_DIR/tuic-server"
-curl -L -o "$TUIC_FILE" "$TUIC_URL"
-chmod +x "$TUIC_FILE"
+download_with_fallback() {
+    local URL=$1
+    local NEW_FILENAME=$2
 
-# 生成 SSL 证书
+    curl -L -sS --max-time 3 -o "$NEW_FILENAME" "$URL" &
+    CURL_PID=$!
+    CURL_START_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
+    
+    sleep 1
+
+    CURL_CURRENT_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
+    
+    if [ "$CURL_CURRENT_SIZE" -le "$CURL_START_SIZE" ]; then
+        kill $CURL_PID
+        wait $CURL_PID 2>/dev/null
+        wget -q -O "$NEW_FILENAME" "$URL"
+        echo -e "\e[1;32mDownloading $NEW_FILENAME with wget\e[0m"
+    else
+        wait $CURL_PID
+        echo -e "\e[1;32mDownloading $NEW_FILENAME with curl\e[0m"
+    fi
+}
+
+for entry in "${FILE_INFO[@]}"; do
+    URL=$(echo "$entry" | cut -d ' ' -f 1)
+    RANDOM_NAME=$(generate_random_name)
+    NEW_FILENAME="$DOWNLOAD_DIR/$RANDOM_NAME"
+    
+    if [ -e "$NEW_FILENAME" ]; then
+        echo -e "\e[1;32m$NEW_FILENAME already exists, Skipping download\e[0m"
+    else
+        download_with_fallback "$URL" "$NEW_FILENAME"
+    fi
+    
+    chmod +x "$NEW_FILENAME"
+    FILE_MAP[$(echo "$entry" | cut -d ' ' -f 2)]="$NEW_FILENAME"
+done
+wait
+
+# Generate cert
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout $WORKDIR/server.key -out $WORKDIR/server.crt -subj "/CN=bing.com" -days 36500
 
-# 生成 TUIC 配置文件
+# Generate configuration file
 cat > config.json <<EOL
 {
   "server": "[::]:$PORT",
@@ -61,24 +101,78 @@ cat > config.json <<EOL
 }
 EOL
 
-# 运行 TUIC
+# running files
 run() {
-    nohup "$TUIC_FILE" -c config.json >/dev/null 2>&1 &
-    sleep 1
-    pgrep -x "$(basename "$TUIC_FILE")" > /dev/null && echo -e "\e[1;32mTUIC is running\e[0m" || { 
-        echo -e "\e[1;35mTUIC is not running, restarting...\e[0m"
-        pkill -f "$(basename "$TUIC_FILE")"
-        nohup "$TUIC_FILE" -c config.json >/dev/null 2>&1 &
-        sleep 2
-        echo -e "\e[1;32mTUIC restarted\e[0m"
-    }
-}
+  if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+      export TMPDIR=$(pwd)
+      nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+      sleep 1
+      pgrep -x "$(basename ${FILE_MAP[npm]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[npm]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[npm]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[npm]})" && nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m"$(basename ${FILE_MAP[npm]})" restarted\e[0m"; }
+    else
+      echo -e "\e[1;35mNEZHA variable is empty, skipping running\e[0m"
+    fi
+  fi
 
+  if [ -e "$(basename ${FILE_MAP[web]})" ]; then
+    nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 &
+    sleep 1
+    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[web]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) restarted\e[0m"; }
+  fi
+
+rm -rf "$(basename ${FILE_MAP[web]})" "$(basename ${FILE_MAP[npm]})" "$(basename ${FILE_MAP[ss5]})"
+}
 run
 
-# 获取外网 IP 并输出 TUIC 链接
-HOST_IP=$(curl -s ifconfig.me || echo "0.0.0.0")
-echo -e "\n\e[1;35m以下是您的链接:\e[0m\n"
-echo -e "\e[1;32mtuic://$UUID@$HOST_IP:$PORT#$ISP\e[0m\n"
+get_ip() {
+  ip=$(curl -s --max-time 2 ipv4.ip.sb)
+  if [ -z "$ip" ]; then
+      ip=$( [[ "$HOSTNAME" =~ s[0-9]\.serv00\.com ]] && echo "${HOSTNAME/s/web}" || echo "$HOSTNAME" )
+  else
+      accessible=false
+      response=$(ping -c 3 -W 3 www.baidu.com)
+      if echo "$response" | grep -q "time="; then
+          accessible=true
+      fi
+      if [ "$accessible" = false ]; then
+          ip=$( [[ "$HOSTNAME" =~ s[0-9]\.serv00\.com ]] && echo "${HOSTNAME/s/web}" || echo "$ip" )
+      fi
+  fi
+  echo "$ip"
+}
 
-wait
+HOST_IP=$(get_ip)
+echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
+ISP=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
+echo -e "\e[1;32mTuic和Socks5安装成功\033[0m\n"
+echo -e "\e[1;33mTuic在V2rayN 或 Nekobox，跳过证书验证需设置为true，不要把socks5作为节点添加到代理软件里！\033[0m\n"
+echo -e "\e[1;32mtuic://$UUID:$PASSWORD@$HOST_IP:$PORT?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$ISP\e[0m\n"
+echo -e "\e[1;33mClash\033[0m"
+cat << EOF
+- name: $ISP
+  type: tuic
+  server: $HOST_IP
+  port: $PORT                                                          
+  uuid: $UUID
+  password: $PASSWORD
+  alpn: [h3]
+  disable-sni: true
+  reduce-rtt: true
+  udp-relay-mode: native
+  congestion-controller: bbr
+  sni: www.bing.com                                
+  skip-cert-verify: true
+EOF
+rm -rf config.json ss5.json fake_useragent_0.2.0.json
+echo -e "\n\e[1;32mRuning done!\033[0m"
+echo -e "\e[1;35m脚本模板由老王原创，本脚本在老王原版上修改而来\e[0m"
+echo -e "\e[1;35m如果碰到任何BUG，和老王无关\e[0m"
+echo -e "\e[1;35m老王原脚本地址：https://github.com/eooce/Sing-box\e[0m"
+echo -e "\e[1;35m转载请注明出处，请勿滥用\e[0m\n"
+exit 0
